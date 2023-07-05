68dcb7dfe8abc8c3cfbbab82eab5ba36f50ef490
shachkovilya <vopros.com@gmail.com>
2023-07-05 14:36:07 +0300

    Обновления лицензий и источников
    Добавлено упоминание банка в авторстве и обновлена лицензия ПО

--- tinkoff-auth/tinkoff-auth.php
+++ tinkoff-auth/tinkoff-auth.php
@@ -4,8 +4,8 @@ Plugin Name: TID
 Plugin URI: https://www.tinkoff.ru/tinkoff-id/
 Description: Плагин авторизации через Тинькофф ID
 Version: 0.6
-Author: Moodmachine
-Author URI: https://moodmachine.ru/
+Author: Tinkoff ID Team
+Author URI: https://www.tinkoff.ru/tinkoff-id/
 */
 
 require_once __DIR__ . '/cms/wordpress.php';
